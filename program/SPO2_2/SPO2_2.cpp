988e325bb4a3ed0a20d84dd00694338b68bce18b
gtcoda <gtcoda@gmail.com>
2020-04-25 23:51:44 +0300

    Настроил коннект с MySQL

--- program/SPO2_2/SPO2_2.cpp
+++ program/SPO2_2/SPO2_2.cpp
@@ -6,6 +6,19 @@ using namespace std;
 #include <fstream>
 #include <string>
 #pragma comment(lib, "wbemuuid.lib")
+#pragma comment(lib,"mysqlcppconn-static.lib")
+#pragma comment(lib,"mysqlcppconn.lib")
+
+
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+#include <ctime>
+
 
 #include "ControlWMI.h"
 #include "SysInfo.h"
@@ -13,6 +26,50 @@ using namespace std;
 
 int main(int iArgCnt, char ** argv)
 {
+	// переходим в консоли на русский язык
+	system("chcp 1251 > nul");
+
+	std::time_t t = std::time(0);  // t is an integer type
+
+try {
+	sql::Driver *driver;
+	sql::Connection *con;
+	sql::Statement *stmt;
+
+
+	driver = get_driver_instance();
+	con = driver->connect("tcp://gtcoda.ru:3306", "oop", "3o4GAIQCHvPDPM6E");
+	/* Connect to the MySQL test database */
+	con->setSchema("test");
+	
+	sql::PreparedStatement *prep_stmt;
+
+	prep_stmt = con->prepareStatement("INSERT INTO test_table(test_char, time) VALUES (?, ?)");
+	prep_stmt->setString(1, "a");
+	prep_stmt->setInt64(2, t);
+	prep_stmt->execute();
+
+	std::string s = "sfdsdfsd";
+
+	sql::SQLString sq = sql::SQLString(s.c_str());
+
+	prep_stmt->setString(1,sq);
+	prep_stmt->setInt64(2, t);
+	prep_stmt->execute();
+
+	delete prep_stmt;
+	delete con;
+
+
+}
+catch (sql::SQLException &e) {
+	cout << "# ERR: SQLException in " << __FILE__;
+	cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+	cout << "# ERR: " << e.what();
+	cout << " (MySQL error code: " << e.getErrorCode();
+	cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+}
+
 
 	// переходим в консоли на русский язык
 	system("chcp 1251 > nul");
